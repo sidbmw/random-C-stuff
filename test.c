24202ba998591c0b99e5a1227def28d82228cddf
sidbmw <siddharthnamtamai@gmail.com>
2020-01-24 22:24:58 -0500

    Hello World!

---
+++ test.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(){
+  //int num, result = 0, i;
+
+  //scanf("%d", &num); //read number from the keyboard
+
+  //for(i - 1; i <= num; i++){ //compute the result
+  //result += i;
+  //}
+
+  //printf("%d\n", result); //output the result
+  printf("Hello, World!\n");
+  return 0;
+}
